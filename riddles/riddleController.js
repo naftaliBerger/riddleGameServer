785f98fd40e1d6011075c42100e8a49fd5053ea1
Naftali Berger <bn26665@gmail.com>
2025-07-10 15:15:21 +0300

    new commit

--- riddles/riddleController.js
+++ riddles/riddleController.js
@@ -24,36 +24,26 @@ export function getRiddleById(req, res) {
 export function createRiddle(req, res) {
   const { name, taskDescription, correctAnswer } = req.body;
   const riddles = readDB();
-  const newRiddle = {
-    id: riddles[riddles.length - 1].id + 1,
-    name,
-    taskDescription,
-    correctAnswer
-  };
+  const newRiddle = {id: riddles[riddles.length - 1].id + 1,name, taskDescription,  correctAnswer };
   riddles.push(newRiddle);
   writeDB(riddles);
   res.status(201).json(newRiddle);
 }
 
 export function updateRiddle(req, res) {
-  const id = +req.params.id;
-  const riddles = readDB();
+    const id = Number(req.params.id);
+    const riddles = readDB();
   const index = riddles.findIndex(r => r.id === id);
 
   const { name, taskDescription, correctAnswer } = req.body;
-  riddles[index] = {
-    id,
-    name,
-    taskDescription,
-    correctAnswer
-  };
+  riddles[index] = {id, name,taskDescription,correctAnswer };
   writeDB(riddles);
   res.json(riddles[index]);
 }
 
 export function deleteRiddle(req, res) {
   const riddles = readDB();
-  const id = +req.params.id;
+  const id = Number(req.params.id);
   const filtered = riddles.filter(r => r.id !== id);
   filtered.forEach((r, i) => r.id = i + 1);
   writeDB(filtered);
