0c8607e8c368f5cc92fcc30b5432d761edfc8dae
Naftali Berger <bn26665@gmail.com>
2025-07-22 09:46:10 +0300

    Add comments

--- DAL/riddlesDAL.js
+++ DAL/riddlesDAL.js
@@ -1,28 +1,30 @@
 import { connectDB } from "../DB/mongodb.js";
 import { ObjectId } from "mongodb";
+
 //--------------------------------------------
+// DAL function to retrieve all riddles from the MongoDB "riddles" collection
 export async function getAllriddels() {
   const db = await connectDB();
   return db.collection("riddles").find().toArray();
 }
+
 //--------------------------------------------
+// DAL function to insert a new riddle into the "riddles" collection
 export async function addriddels(report) {
   const db = await connectDB();
   return db.collection("riddles").insertOne(report);
 }
+
 //--------------------------------------------
+// DAL function to update a specific riddle by its ID
 export async function updateRiddelById(id, product) {
   const db = await connectDB();
-  return db.collection("riddles").updateOne({ _id: new ObjectId(id) },{ $set: product });
+  return db.collection("riddles").updateOne({ _id: new ObjectId(id) }, { $set: product });
 }
+
 //--------------------------------------------
+// DAL function to delete a riddle by its ID from the "riddles" collection
 export async function deleteRiddleById(id) {
   const db = await connectDB();
   return db.collection("riddles").deleteOne({ _id: new ObjectId(id) });
 }
-
-
-
-
-
-
--- router/playersRouter.js
+++ router/playersRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {getPlayers, createPlayer} from '../controllers/playersController.js';
+import {getPlayers, createPlayer,updatePlayer} from '../controllers/playersController.js';
 
 const router = express.Router();
 router.get('/', getPlayers);
 router.post('/', createPlayer);
+router.put('/:id', updatePlayer);
 
 export default router;
