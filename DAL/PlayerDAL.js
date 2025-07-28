c99bbd3c88bc2b9e8bb7b97b6c1aaa9d4ed0aa95
Naftali Berger <bn26665@gmail.com>
2025-07-28 10:57:36 +0300

    fixe

--- DAL/PlayerDAL.js
+++ DAL/PlayerDAL.js
@@ -16,11 +16,11 @@ export async function insertPlayer(playerData) {
 //--------------------------------------------
 // DAL function to update an existing player by ID in the "Player" table
 export async function updatePlayerById(id, playerData) {
-    return await Supabase.from("players").update(playerData).eq("id", id).select().single();     
+    return await Supabase.from("players").update(playerData).eq("id", id);     
 }
 //--------------------------------------------
 export async function getUserByUsername(username) {
-  const { data, error } = await Supabase.from("players").select("*").eq("username", username).single();
+  const { data, error } = await Supabase.from("players").select("*").eq("username", username);
   return { data, error };
 }
 //--------------------------------------------
@@ -30,7 +30,7 @@ export async function checkPassword(plainPassword, hashedPassword) {
 //--------------------------------------------
 export async function updatePlayerByUsername(username, updateFields) {
   try {
-    const { data, error } = await Supabase.from("players").update(updateFields).eq("username", username).select();
+    const { data, error } = await Supabase.from("players").update(updateFields).eq("username", username);
     return { data, error };
   } catch (error) {
     return { data: null, error };
